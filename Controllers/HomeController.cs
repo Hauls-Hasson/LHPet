0617cbc04a52c8dfb70f30be7b47b7ca5f632877
Ismael Rodrigues de Sousa <58527984+Hauls-Hasson@users.noreply.github.com>
2024-05-06 03:24:27 -0300

    Procedimento de criação de listas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,18 +22,25 @@ Cliente cliente3 = new Cliente(03, "Ada Lovelace","800.777.920-50", "ada@ada.lan
 Cliente cliente4 = new Cliente(04, "Linus Torvalds", "933.622.400-03", "torvalds@osdl.org", "Pinguim");
 Cliente cliente5 = new Cliente(05, "Grace Hopper", "911.702.988-00", "grace.hopper@cobol.com", "Loboc");
 
-Fornecedor fornecedor1 = new Fornecedor(01, "C#PET S/A", "14.182.102/0001-80", "c-sharp@pet.org");
-Fornecedor fornecedor2 = new Fornecedor(02, "CtrlAlt Dog", "15.836.698/0001-57", "ctrl@alt.dog.br");
-Fornecedor fornecedor3 = new Fornecedor(03, "BootsPet INC", "40.810.224/0001-83", "boots.pet@gatomania.us");
-Fornecedor fornecedor4 = new Fornecedor(04, "TikTok Dogs", "87.945.350/0001-09","noisnamidia@tiktokdogs.uk");
-Fornecedor fornecedor5 = new Fornecedor(05, "Bifinho Forever", "18.760.614/0001-37", "contato@bff.us");
-
 List<Cliente> listaClientes = new List<Cliente>();
 listaClientes.Add(cliente1);
 listaClientes.Add(cliente2);
 listaClientes.Add(cliente3);
 listaClientes.Add(cliente4);
 listaClientes.Add(cliente5);
+
+Fornecedor fornecedor1 = new Fornecedor(01, "C#PET S/A", "14.182.102/0001-80", "c-sharp@pet.org");
+Fornecedor fornecedor2 = new Fornecedor(02, "CtrlAlt Dog", "15.836.698/0001-57", "ctrl@alt.dog.br");
+Fornecedor fornecedor3 = new Fornecedor(03, "BootsPet INC", "40.810.224/0001-83", "boots.pet@gatomania.us");
+Fornecedor fornecedor4 = new Fornecedor(04, "TikTok Dogs", "87.945.350/0001-09","noisnamidia@tiktokdogs.uk");
+Fornecedor fornecedor5 = new Fornecedor(05, "Bifinho Forever", "18.760.614/0001-37", "contato@bff.us");
+
+List<Fornecedor> listaFornecedores = new List<Fornecedor>();
+listaFornecedores.Add(fornecedor1);
+listaFornecedores.Add(fornecedor2);
+listaFornecedores.Add(fornecedor3);
+listaFornecedores.Add(fornecedor4);
+listaFornecedores.Add(fornecedor5);
 
         return View();
     }
